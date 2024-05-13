6aef92e41b90a0e8bab27a3a4f7bf65bab82564a
zephyr <i@zephyr.moe>
2024-05-13 19:46:30 +0900

    chapter12

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,24 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useRouter, usePathname, useSearchParams } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
+import { Z_VERSION_ERROR } from 'zlib';
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const { replace } = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  function handleSearch(s: string) {
+    const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
+    if (s) { params.set('query', s); } else { params.delete('query'); }
+    replace(`${pathname}?${params.toString()}`);
+  }
+
+  const handleSearch2 = useDebouncedCallback(s => handleSearch(s), 500);
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -11,6 +27,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        defaultValue={searchParams.get('query')?.toString()}
+        onChange={(e) => handleSearch2(e.target.value)}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
